e74e18a4f5559ba5fa8d9feb5a0ed353ac445d4d
sijan67 <sijuichi1@gmail.com>
2023-11-17 14:09:54 -0800

    Completed dashboard screen UI

--- frontend/CollabUpApp/screens/DashboardPage/DashboardPage.js
+++ frontend/CollabUpApp/screens/DashboardPage/DashboardPage.js
@@ -6,9 +6,15 @@ import {
   ScrollView,
   StyleSheet,
   ActivityIndicator,
-  Text
+  Text,
+  TextInput,
+  Button,
+  View,
+  Image,
+  TouchableHighlight
 } from 'react-native';
-
+import * as ImagePicker from 'expo-image-picker';
+import { FontAwesome } from '@expo/vector-icons';
 
 
 // import { PostItem  } from "../../components/PostItem";
@@ -18,6 +24,8 @@ import PostItem from "../../components/PostItem";
 export default function Dashboard() {
   const [isRefreshing, setIsRefreshing] = useState(false);
   const { posts } = usePostContext();
+  const [newPost, setNewPost] = useState({ title: '', skills: '', idea: '', image: '' });
+  const [image, setImage] = useState(null);
 
 
   useEffect(() => {
@@ -33,6 +41,39 @@ export default function Dashboard() {
 
     // After completing the refresh operation, set refreshing to false
     setIsRefreshing(false);
+  };
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      setNewPost({ ...newPost, image: result.assets[0].uri });
+    }
+  };
+
+  const handleCreatePost = () => {
+    // Add logic to create a new post
+
+    // TO DOO
+    // addPost(newPost);
+
+    // Reset the newPost state after creating a post
+    setNewPost({ title: '', skills: '', idea: '', image: '' });
+    setImage(null)
+  };
+
+  const isFormComplete = () => {
+    // Check if all fields in the newPost state are filled
+    return Object.values(newPost).every((value) => value.trim() !== '');
   };
 
   return (
@@ -51,6 +92,56 @@ export default function Dashboard() {
         }
       >
         {isRefreshing && <ActivityIndicator size="large" color="#0000ff" />}
+      
+        <View style={{backgroundColor: "#D9E2E7", borderRadius: 30, padding: 20, margin: 10}}>
+          <View style={{flexDirection: 'row',}}>
+          <Text> Title </Text>
+          <TextInput
+            placeholder="Title"
+            value={newPost.title}
+            onChangeText={(text) => setNewPost({ ...newPost, title: text })}
+            style={{marginLeft: 10}}
+          />
+          </View>
+         
+          <View style={{flexDirection: 'row', marginTop: 10}}>
+            <Text> Skills </Text>
+            <TextInput
+              placeholder="Skills"
+              value={newPost.skills}
+              onChangeText={(text) => setNewPost({ ...newPost, skills: text })}
+              style={{marginLeft: 6}}
+            />
+          </View>
+
+          
+        <TextInput
+          placeholder="What's your project / start up idea ? "
+          value={newPost.idea}
+          onChangeText={(text) => setNewPost({ ...newPost, idea: text })}
+          style={{marginTop: 15, fontSize: 20 , textAlign: 'center' }}
+        />
+       
+
+       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          {image && <Image source={{ uri: image }} style={{ width: 200, height: 200, marginTop: 5 }} />}
+
+          <TouchableHighlight style={{marginTop: 10}} onPress={pickImage} >
+              <View>
+              <FontAwesome name="image" size={24} color="#0779B8" />
+              
+              </View>
+          </TouchableHighlight>
+         
+        </View>
+
+        {/* Button to create a new post */}
+        <Button title="Create Post" 
+        onPress={handleCreatePost} 
+        disabled={!isFormComplete()}
+         />
+
+        </View>
         
         {posts.map((post) => (
           <PostItem key={post.id} post={post} />
